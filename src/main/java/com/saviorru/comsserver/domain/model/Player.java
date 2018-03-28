ddcd0f9e200550e4ba53ddcce3be161d0caadec6
Vitaliy <Gallifrey46@gmail.com>
2018-03-28 20:22:29 +0300

    Добавлено возрастное ограничение для игрока.

--- src/main/java/com/saviorru/comsserver/domain/model/Player.java
+++ src/main/java/com/saviorru/comsserver/domain/model/Player.java
@@ -15,6 +15,7 @@ public class Player {
     public Player(String firstName, String lastName, LocalDate birthDate){
         if (firstName == null || lastName == null || birthDate == null) throw new NullPointerException();
         if (firstName.isEmpty() || lastName.isEmpty()) throw new IllegalArgumentException("Name of surname can't be empty");
+        if(LocalDate.now().getYear() - 7 < birthDate.getYear()) throw new IllegalArgumentException("The player is too young");
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate;
@@ -23,6 +24,7 @@ public class Player {
     public Player(String firstName, String lastName, String patronymicName, LocalDate birthDate){
         if (firstName == null || lastName == null || birthDate == null) throw new NullPointerException();
         if (firstName.isEmpty() || lastName.isEmpty()) throw new IllegalArgumentException("Name of surname can't be empty");
+        if(LocalDate.now().getYear() - 7 < birthDate.getYear()) throw new IllegalArgumentException("The player is too young");
         this.firstName = firstName;
         this.lastName = lastName;
         this.patronymicName = patronymicName;
