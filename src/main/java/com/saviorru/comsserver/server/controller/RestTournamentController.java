ad5157eec90aea5b7816a26536252a9ee88f5fb6
Vitaliy <Gallifrey46@gmail.com>
2018-04-06 14:13:27 +0300

    Добавлена структура для сервера и классы

--- src/main/java/com/saviorru/comsserver/server/controller/RestTournamentController.java
+++ src/main/java/com/saviorru/comsserver/server/controller/RestTournamentController.java
@@ -1,4 +1,4 @@
-package com.saviorru.comsserver;
+package com.saviorru.comsserver.server.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
